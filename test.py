4f470b12b8764e8f729e1838abb26d6b00bebe1f
egor <troyanviskov@gmail.com>
2023-11-27 21:59:18 +0300

    new file:   __pycache__/boss.cpython-310.pyc new file:   __pycache__/hero.cpython-310.pyc new file:   images/hero2attackpixel.png new file:   images/hero2blockpixel.png new file:   images/hero2shiftpixel.png new file:   images/hero2spacepixel.png new file:   images/mario-background.jpg modified:   test.py

--- test.py
+++ test.py
@@ -8,14 +8,14 @@ FPS = 60
 COOLDOWN_TIME = 1  # Кулдаун в секундах
 GRAVITY = 0.75  # Гравитация 
 
-JUMP_HEIGHT = 20  # Высота прыжка
-# JUMP_COOLDOWN = 20  # Кулдаун для прыжков в кадрах
-# JUMP_SPEED = 10  # Скорость подпрыгивания
+JUMP_HEIGHT = 10  # Высота прыжка
+JUMP_COOLDOWN = 50  # Кулдаун для прыжков в кадрах
+JUMP_SPEED = 1  # Скорость подпрыгивания
 
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
-PLATFORM_HEIGHT = 15
+PLATFORM_HEIGHT = 30
 PLAYER_HEALTH = 100
 
 # Установка окна
@@ -25,7 +25,7 @@ clock = pygame.time.Clock()
 
 # Загрузка изображений персонажей
 player1_images = {
-    "idle": pygame.image.load("images/hero1.png"),
+    "idle": pygame.image.load("images/hero3.png"),
     "attack": pygame.image.load("images/hero3.png"),
     "jump": pygame.image.load("images/hero3.png"),
     "crouch": pygame.image.load("images/hero3.png"),
@@ -37,25 +37,62 @@ player2_images = {
     "jump": pygame.image.load("images/hero2.png"),
     "crouch": pygame.image.load("images/hero2.png"),
 }
+background_image = pygame.image.load("images/mario-background.jpg")
+background_image = pygame.transform.scale(background_image, (WIDTH, HEIGHT))
 
+
+GROUND_LEVEL = HEIGHT - PLATFORM_HEIGHT - player2_images["idle"].get_height() 
 def draw_players():
+    global player1_state
+    global player2_state
+
+    # Handle keyboard events
+    for event in pygame.event.get():
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                player1_state = "jump"
+            elif event.key == pygame.K_LCTRL:
+                player1_state = "crouch"
+            elif event.key == pygame.K_RETURN:
+                player1_state = "attack"
+
+
     if player1_state == "jump":
         player1_image = player1_images["jump"]
+
     elif player1_state == "crouch":
         player1_image = player1_images["crouch"]
+        
     elif player1_state == "attack":
         player1_image = player1_images["attack"]
+        
     else:
         player1_image = player1_images["idle"]
+        
 
     if player2_state == "jump":
         player2_image = player2_images["jump"]
+       
+        if player2.y > GROUND_LEVEL - JUMP_HEIGHT:
+            player2.y -= JUMP_SPEED
+        else:
+            player2_state = "idle"  
+
+        
+        if player2.y >= GROUND_LEVEL:
+            player2.y = GROUND_LEVEL
+            player2_state = "idle"  
+
     elif player2_state == "crouch":
         player2_image = player2_images["crouch"]
+       
     elif player2_state == "attack":
         player2_image = player2_images["attack"]
+        
     else:
         player2_image = player2_images["idle"]
+       
+
 
     scaled_player1_image = pygame.transform.scale(player1_image, (player1.width, player1.height))
     screen.blit(scaled_player1_image, player1)
@@ -63,10 +100,6 @@ def draw_players():
     scaled_player2_image = pygame.transform.scale(player2_image, (player2.width, player2.height))
     screen.blit(scaled_player2_image, player2)
 
-# Игровые объекты
-player1 = pygame.Rect(50, HEIGHT - 100, 50, 50)
-player2 = pygame.Rect(200, HEIGHT - 100, 50, 50)
-players = [player1, player2]
 
 # Платформа
 platform = pygame.Rect(0, HEIGHT - PLATFORM_HEIGHT, WIDTH, PLATFORM_HEIGHT)
@@ -90,13 +123,13 @@ player1_state = "idle"  # начальное состояние
 player2_state = "idle"  # начальное состояние
 
 # # Игровые объекты и переменные для прыжка
-# player1 = pygame.Rect(50, HEIGHT - 100, 50, 50)
-# player2 = pygame.Rect(200, HEIGHT - 100, 50, 50)
-# players = [player1, player2]
-# is_jumping = [False, False]
-# jump_count = [JUMP_HEIGHT, JUMP_HEIGHT]
-# jump_cooldown = [0, 0]
-# jump_speed = [0, 0]
+player1 = pygame.Rect(50, HEIGHT - 100, 50, 50)
+player2 = pygame.Rect(200, HEIGHT - 100, 50, 50)
+players = [player1, player2]
+is_jumping = [False, False]
+jump_count = [JUMP_HEIGHT, JUMP_HEIGHT]
+jump_cooldown = [0, 0]
+jump_speed = [0, 0]
 
 # Основной игровой цикл
 running = True
@@ -119,30 +152,31 @@ while running:
     if keys[pygame.K_s] and not is_jumping[0]:
         player1.y += 5 
         player1_state = "crouch"
-    # if keys[pygame.K_w] and not is_jumping[0] and jump_cooldown[0] == 0:
-    #     is_jumping[0] = True
-    #     jump_speed[0] = JUMP_SPEED
-    #     jump_cooldown[0] = JUMP_COOLDOWN
-    #     player1_state = "jump"
+    if keys[pygame.K_w] and not is_jumping[0] and jump_cooldown[0] == 0:
+        is_jumping[0] = True
+        jump_speed[0] = JUMP_SPEED
+        jump_cooldown[0] = JUMP_COOLDOWN
+        player1_state = "jump"
 
-    # if is_jumping[0]:
-    #     if jump_count[0] >= -JUMP_HEIGHT:
-    #         player1.y -= (jump_count[0] ** 2) * 0.5 * GRAVITY
-    #         jump_count[0] -= 1
-    #     else:
-    #         is_jumping[0] = False
-    #         jump_count[0] = JUMP_HEIGHT
+    if is_jumping[0]:
+        if jump_count[0] >= -JUMP_HEIGHT:
+           player1.y -= (jump_count[0] ** 2) * 0.5 * GRAVITY
+           jump_count[0] -= 1
+        else:
+            is_jumping[0] = False
+            jump_count[0] = JUMP_HEIGHT
 
-    # for i in range(2):
-    #     if is_jumping[i]:
-    #         if jump_speed[i] >= -JUMP_SPEED:
-    #             players[i].y -= jump_speed[i]
-    #             jump_speed[i] -= 1
-    #         else:
-    #             is_jumping[i] = False
-    #             jump_speed[i] = 0  # Сброс скорости
-    #     elif players[i].colliderect(platform):
-    #         jump_cooldown[i] = JUMP_COOLDOWN  # Установка кулдауна
+    for i in range(2):
+        if is_jumping[i]:
+             if jump_speed[i] >= -JUMP_SPEED:
+                 players[i].y -= jump_speed[i]
+                 jump_speed[i] -= 1
+        else:
+               is_jumping[i] = False
+               jump_speed[i] = 0  # Сброс скорости
+
+        if players[i].colliderect(platform):
+            jump_cooldown[i] = JUMP_COOLDOWN  # Установка кулдауна
 
 
 
@@ -157,18 +191,18 @@ while running:
     player2.x += enemy_speed * move_direction
 
     # # Добавляем прыжок для синего квадрата при приближении красного
-    # if player1.colliderect(player2) and not is_jumping[1]:
-    #     is_jumping[1] = True
-    #     jump_count[1] = JUMP_HEIGHT
-    #     player2_state = "jump"
+    if player1.colliderect(player2) and not is_jumping[1]:
+        is_jumping[1] = True
+        jump_count[1] = JUMP_HEIGHT
+        player2_state = "jump"
 
-    # if is_jumping[1]:
-    #     if jump_count[1] >= -JUMP_HEIGHT:
-    #         player2.y -= (jump_count[1] ** 2) * 0.5 * GRAVITY
-    #         jump_count[1] -= 1
-    #     else:
-    #         is_jumping[1] = False
-    #         jump_count[1] = JUMP_HEIGHT
+    if is_jumping[1]:
+        if jump_count[1] >= -JUMP_HEIGHT:
+             player2.y -= (jump_count[1] ** 2) * 0.5 * GRAVITY
+             jump_count[1] -= 1
+        else:
+             is_jumping[1] = False
+             jump_count[1] = JUMP_HEIGHT
 
     # Добавляем приседание для синего квадрата при приближении красного
     if player1.colliderect(player2) and not is_jumping[1]:
@@ -186,16 +220,18 @@ while running:
         cooldowns[2] = FPS * COOLDOWN_TIME  # Установка Cooldown'а
 
     # Отрисовка
-    screen.fill(WHITE)
+    screen.blit(background_image, (0, 0))
     draw_players()
     pygame.draw.rect(screen, (0, 0, 0), platform)
 
     # Отображение здоровья
+    
     font = pygame.font.Font(None, 36)
-    text = font.render(f"Player 1 Health: {health[0]}", True, RED)
+    text = font.render(f"Player 1 Health: {health[0]}", True, RED) 
     screen.blit(text, (10, 10))
     text = font.render(f"Player 2 Health: {health[1]}", True, BLUE)
     screen.blit(text, (WIDTH - text.get_width() - 10, 10))
+   
 
     pygame.display.flip()
 
